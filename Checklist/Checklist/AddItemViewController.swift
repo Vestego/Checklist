6b3b8cf1b28483505c3c88262bea039e196fe702
Eoghan <eoghan@macbook-pro.local>
2018-05-26 00:41:56 +0200

    Completed Chapter 12 Tough but some good notes

--- Checklist/Checklist/AddItemViewController.swift
+++ Checklist/Checklist/AddItemViewController.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 
-class AddItemViewController: UITableViewController {
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
     
     @IBOutlet weak var textField: UITextField!
     
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,4 +41,19 @@ class AddItemViewController: UITableViewController {
         //this determines what field the keyboard focus will be on
         textField.becomeFirstResponder()
     }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let oldText = textField.text!
+        let stringRange = Range(range, in:oldText)!
+        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+        
+        if newText.isEmpty {
+            doneBarButton.isEnabled = false
+        } else {
+            doneBarButton.isEnabled = true
+        }
+        return true
+    }
+    
+    
 }
