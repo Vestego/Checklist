6510d62aedcfa05a6b6a2c121d089d1c6b89c1d2
Eoghan <eoghan@macbook-pro.local>
2018-05-26 00:05:56 +0200

    Completed Page 347 - Eoghan

--- Checklist/Checklist/AddItemViewController.swift
+++ Checklist/Checklist/AddItemViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class AddItemViewController: UITableViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -19,6 +23,21 @@ class AddItemViewController: UITableViewController {
     }
     
     @IBAction func done() {
+        print("Contents of the text field: \(textField.text!)")
         navigationController?.popViewController(animated: true)
+        
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
+    }
+    
+    //Makes the keyboard appear when the screen appears (already in the text box)
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //this determines what field the keyboard focus will be on
+        textField.becomeFirstResponder()
     }
 }
