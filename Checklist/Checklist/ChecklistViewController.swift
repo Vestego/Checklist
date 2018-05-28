da515b63c940c278dcce5819b8dc067f8d06745f
Eoghan <eoghan@macbook-pro.local>
2018-05-28 18:04:43 +0100

    End of Page 381

--- Checklist/Checklist/ChecklistViewController.swift
+++ Checklist/Checklist/ChecklistViewController.swift
@@ -88,10 +88,12 @@ class ChecklistViewController: UITableViewController, AddItemViewControllerDeleg
     
 
     func configureCheckmark(for cell: UITableViewCell, with item: ChecklistItem) {
+        let label = cell.viewWithTag(1001) as! UILabel
+        
         if item.checked {
-            cell.accessoryType = .checkmark
+            label.text = "√"
         } else {
-            cell.accessoryType = .none
+            label.text = ""
         }
     }
     
