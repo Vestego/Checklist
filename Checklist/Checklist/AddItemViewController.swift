ee7bd0c3e5e99ff3d86b056a72fde90e869ba071
Eoghan <eoghan@macbook-pro.local>
2018-06-01 02:30:07 +0100

    Page 381 - Part 2 Totally fucked up everything, had to pull ALL code down again

--- Checklist/Checklist/AddItemViewController.swift
+++ Checklist/Checklist/AddItemViewController.swift
@@ -1,29 +1,32 @@
-
+//
+//  AddItemViewController.swift
+//  Checklist
+//
+//  Created by Eoghan on 2018-05-25.
+//  Copyright © 2018 Vestego. All rights reserved.
+//
 
 import UIKit
 
 protocol AddItemViewControllerDelegate: class {
     func addItemViewControllerDidCancel(_ controller: AddItemViewController)
-    func addItemViewController(_ controller: AddItemViewController,
-                               didFinishAdding item: ChecklistItem)
+    func addItemViewController(_ controller: AddItemViewController, didFinishAdding item: ChecklistItem)
 }
 
+
+
+
 class AddItemViewController: UITableViewController, UITextFieldDelegate {
+    
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var doneBarButton: UIBarButtonItem!
     weak var delegate: AddItemViewControllerDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationItem.largeTitleDisplayMode = .never
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        textField.becomeFirstResponder()
-    }
     
-    // MARK:- Actions
     @IBAction func cancel() {
         delegate?.addItemViewControllerDidCancel(self)
     }
@@ -32,24 +35,33 @@ class AddItemViewController: UITableViewController, UITextFieldDelegate {
         let item = ChecklistItem()
         item.text = textField.text!
         item.checked = false
-        
         delegate?.addItemViewController(self, didFinishAdding: item)
     }
     
-    // MARK:- TableView Delegates
+    
     override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
         return nil
     }
     
-    // MARK:- UITextField Delegates
-    func textField(_ textField: UITextField,
-                   shouldChangeCharactersIn range: NSRange,
-                   replacementString string: String) -> Bool {
+    //Makes the keyboard appear when the screen appears (already in the text box)
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         
+        //this determines what field the keyboard focus will be on
+        textField.becomeFirstResponder()
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         let oldText = textField.text!
         let stringRange = Range(range, in:oldText)!
         let newText = oldText.replacingCharacters(in: stringRange, with: string)
-        doneBarButton.isEnabled = !newText.isEmpty
+        
+        if newText.isEmpty {
+            doneBarButton.isEnabled = false
+        } else {
+            doneBarButton.isEnabled = true
+        }
         return true
     }
+    
 }
