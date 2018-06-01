1f31f39732f4dbcb4f29cb41f772d4ebebe909a4
Eoghan <eoghan@macbook-pro.local>
2018-06-01 20:46:13 +0100

    End of Page 465

--- Checklist/Checklist/ListDetailViewController.swift
+++ Checklist/Checklist/ListDetailViewController.swift
@@ -18,6 +18,7 @@ protocol ListDetailViewControllerDelegate: class {
 
 class ListDetailViewController: UITableViewController, UITextFieldDelegate {
     @IBOutlet weak var textField: UITextField!
+
     @IBOutlet weak var doneBarButton: UIBarButtonItem!
     
     weak var delegate: ListDetailViewControllerDelegate?
